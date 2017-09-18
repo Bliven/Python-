ac0bc52f618fe85e8782939627a619be94b9ed89
niu bin <1147713149@qq.com>
2017-09-18 15:24:59 +0800

    CBV初始版本

---
+++ appCBV/views.py
@@ -0,0 +1,23 @@
+from django.shortcuts import render,HttpResponse
+
+from django.views import View
+
+# Create your views here.
+
+class IndexView(View):
+
+    def dispatch(self, request, *args, **kwargs):
+        print("before")
+        res=super(IndexView,self).dispatch(request,*args,**kwargs)
+        print("after")
+        return res
+
+
+    def get(self,request,*args,**kwargs):
+
+        return render(request,"index.html")
+    
+    def post(self,request,*args,**kwargs):
+
+        return HttpResponse("66666666")
+
